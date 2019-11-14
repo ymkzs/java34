ef3a130e7372c6c65e2998d53471a0d3f80bb06f
zhanglala0 <zjl090501@126.com>
2019-11-14 16:33:13 +0800

    update A

--- src/aa/a.java
+++ src/aa/a.java
@@ -2,4 +2,10 @@ package aa;
 
 public class a{
 	int a;
+	public static void main(String[] args) {
+		System.out.println("zhanglala  yici  xiugai  1111");
+		System.out.println("frfr");
+		System.out.println("2222");
+	}
+	
 }
